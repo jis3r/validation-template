831a1929cbbb60576ffec9b0a616f0fbfbc66a10
jis3r <jis3r@protonmail.com>
2023-05-30 23:27:24 +0200

    fix: testimonials styling and added new components

--- src/lib/components/testimonials.svelte
+++ src/lib/components/testimonials.svelte
@@ -20,7 +20,7 @@
 	});
 </script>
 
-<section class="bg-white">
+<section>
 	<div class="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 sm:py-24 lg:px-8">
 		<h2 class="text-center text-4xl font-bold tracking-tight sm:text-5xl">
 			Read trusted reviews from our customers
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,16 @@
 	import Navigation from '../lib/components/navigation.svelte';
 	import Hero from '../lib/components/hero.svelte';
 	import Testimonials from '../lib/components/testimonials.svelte';
+	import Features from '../lib/components/features.svelte';
+	import Footer from '../lib/components/footer.svelte';
 </script>
 
 <Navigation />
 
 <Hero />
 
+<Features />
+
 <Testimonials />
+
+<Footer />
