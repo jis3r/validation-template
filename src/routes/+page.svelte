2289e786125d24b2ede38f4919c6ac26a055dab0
jis3r <jis3r@protonmail.com>
2023-05-30 23:11:00 +0200

    add: general html-styling and component-imports

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Navigation from '../lib/components/navigation.svelte';
+	import Hero from '../lib/components/hero.svelte';
+	import Testimonials from '../lib/components/testimonials.svelte';
+</script>
+
+<Navigation />
+
+<Hero />
+
+<Testimonials />
